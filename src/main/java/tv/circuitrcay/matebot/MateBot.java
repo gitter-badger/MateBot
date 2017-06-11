9a9ddff4ce6b0617d05dea2610cf43ef060a6080
Duale Siad <dmanrazor@hotmail.com>
2017-06-11 22:28:44 +1000

    Create MateBot.java

--- src/main/java/tv/circuitrcay/matebot/MateBot.java
+++ src/main/java/tv/circuitrcay/matebot/MateBot.java
@@ -7,20 +7,24 @@ import java.lang.String;
 
 import javax.security.auth.login.LoginException;
 
-import org.json.JSONObject;
-
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+
+import org.json.JSONObject;
+
+
 import tv.circuitrcay.matebot.commands.InfoCommand;
 import tv.circuitrcay.matebot.music.Music;
+
 
 public class MateBot {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		try {
 			String config = new String(Files.readAllBytes(Paths.get("config/bot.json")));
 			JSONObject configJson = new JSONObject(config);
@@ -30,21 +34,24 @@ public class MateBot {
 					.setToken(botToken)
 					.addEventListener(new InfoCommand())
 					.addEventListener(new Music())
+                    			.setGame(Game.of(game))
 					.setBulkDeleteSplittingEnabled(false)
-					.buildBlocking();
-			jda.getPresence().setGame(Game.of(game));
+					.buildAsync();
 		} catch(IOException e) {
 			System.out.println("Bot config.json not found!");
 		} catch (LoginException e) {
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+            		e.printStackTrace();
 		} catch (RateLimitedException e) {
 			e.printStackTrace();
 		}
-		
+
+
+
+
+    }
 }
-	}
+
+
 
